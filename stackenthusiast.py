f1ad150794726af746eba86eff5af49affa42a2f
Wiktor Owczarz <Wiktor@wiktor.server.frederix-hotspot.de>
2018-02-16 16:48:50 +0100

    Script to log in with SO credentials.

--- stackenthusiast.py
+++ stackenthusiast.py
@@ -3,9 +3,28 @@ StackEnthusiast
 
 @author: Wiktor Wednesday 7 Feb 2018
 """
+import mechanize
+import credentialsSO
 
-def stack_login():
-    """
-    Functon will allow to login on StackOverflow
-    https://stackoverflow.com/users/login
-    """
+SO_cred = credentialsSO.credentials()
+
+def open_stack(email, password):
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent', 'Safari')]
+    br.open('https://stackoverflow.com/users/login')
+    br.select_form(nr = 1)
+    br.form['email'] = email
+    br.form['password'] = password
+    sub = br.submit()
+    print sub.geturl()
+
+
+def main():
+    email = SO_cred.email
+    password = SO_cred.password
+
+    return open_stack(email, password)
+
+if __name__ == '__main__':
+    main()
